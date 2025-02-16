41b917e17e8cce255a37f603af17a059140f5002
Venkatact <venkatactaladi@gmail.com>
2025-02-17 05:10:49 +0530

    Updated deploy.sh in dev branch.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 Updating some code to check dev branch file.
 
+See if same code reflects in the master branch.
+
 Added some DB code of bullshit to this file.
 
 This file contains the deployment script...
